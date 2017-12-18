fc086464d5fddca0246b9e6248c6d78b070de188
knutaf <knutaf@gmail.com>
2017-12-17 23:29:57 -0800

    day 1 part 2

--- p01/src/main.rs
+++ p01/src/main.rs
@@ -1,4 +1,5 @@
 use std::io::prelude::*;
+use std::env;
 
 fn get_input_from_stdin() -> String {
     let mut contents = String::new();
@@ -22,9 +23,32 @@ fn solve_a(input : &str) -> u32 {
     sum
 }
 
+fn solve_b(input : &str) -> u32 {
+    let digits : Vec<u8> = input.chars().map(|c| { c.to_digit(10).expect("failed to parse digit") as u8 }).collect();
+    let lookahead_offset = digits.len() / 2;
+
+    let sum = digits.iter().enumerate().fold(0u32, |running_sum, (i, &val)| {
+        running_sum + if val == digits[(i + lookahead_offset) % digits.len()] {
+            val as u32
+        } else {
+            0u32
+        }
+    });
+
+    sum
+}
+
 fn main() {
     let input = get_input_from_stdin();
-    let answer = solve_a(input.trim());
+    let input_trimmed = input.trim();
+
+    let answer;
+    if env::args().len() < 2 {
+        answer = solve_a(input_trimmed);
+    } else {
+        answer = solve_b(input_trimmed);
+    }
+
     println!("answer: {}", answer);
 }
 
@@ -50,5 +74,30 @@ mod test {
     #[test]
     fn a_4() {
         assert_eq!(solve_a("91212129"), 9);
+    }
+
+    #[test]
+    fn b_1() {
+        assert_eq!(solve_b("1212"), 6);
+    }
+
+    #[test]
+    fn b_2() {
+        assert_eq!(solve_b("1221"), 0);
+    }
+
+    #[test]
+    fn b_3() {
+        assert_eq!(solve_b("123425"), 4);
+    }
+
+    #[test]
+    fn b_4() {
+        assert_eq!(solve_b("123123"), 12);
+    }
+
+    #[test]
+    fn b_5() {
+        assert_eq!(solve_b("12131415"), 4);
     }
 }
