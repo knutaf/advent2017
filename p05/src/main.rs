3e94eada38f53840b3f425c73788ab980d7f4e07
knutaf <knutaf@gmail.com>
2017-12-30 11:12:35 -0800

    day 5 part 2

--- p05/src/main.rs
+++ p05/src/main.rs
@@ -14,23 +14,44 @@ impl Instruction {
         }
     }
 
-    fn take_jump(&mut self) -> i32 {
+    fn take_jump_a(&mut self) -> i32 {
+        let ret = self.offset + self.modifier;
         self.modifier = self.modifier + 1;
-        self.offset + self.modifier - 1
+        ret
+    }
+
+    fn take_jump_b(&mut self) -> i32 {
+        let ret = self.offset + self.modifier;
+
+        if ret >= 3 {
+            self.modifier = self.modifier - 1;
+        } else {
+            self.modifier = self.modifier + 1;
+        }
+
+        ret
     }
 }
 
 struct Program {
     instructions : Vec<Instruction>,
     ip : i32,
+    mode_a : bool,
 }
 
 impl Program {
-    fn new(instructions : Vec<Instruction>) -> Program {
+    fn new(instructions : Vec<Instruction>, mode_a : bool) -> Program {
         Program {
             instructions : instructions,
             ip : 0,
+            mode_a: mode_a,
         }
+    }
+
+    fn from_input(input : &str, mode_a : bool) -> Program {
+        Program::new(input.lines().map(|line| {
+            Instruction::new(line.parse::<i32>().unwrap())
+        }).collect(), mode_a)
     }
 }
 
@@ -39,11 +60,18 @@ impl Iterator for Program {
 
     fn next(&mut self) -> Option<Self::Item> {
         if self.ip >= 0 && self.ip < (self.instructions.len() as i32) {
-            let next_ip = self.ip + self.instructions.get_mut(self.ip as usize).unwrap().take_jump();
-            self.ip = next_ip;
+            {
+                let instruction = self.instructions.get_mut(self.ip as usize).unwrap();
+                self.ip = self.ip +
+                    if self.mode_a {
+                        instruction.take_jump_a()
+                    } else {
+                        instruction.take_jump_b()
+                    };
+            }
 
             if self.ip >= 0 && self.ip < (self.instructions.len() as i32) {
-                Some(next_ip)
+                Some(self.ip)
             } else {
                 None
             }
@@ -54,16 +82,13 @@ impl Iterator for Program {
 }
 
 fn solve_a(input : &str) -> u32 {
-    let instructions = input.lines().map(|line| {
-        Instruction::new(line.parse::<i32>().unwrap())
-    }).collect();
-
-    let program = Program::new(instructions);
+    let program = Program::from_input(input, true);
     (program.count() + 1) as u32
 }
 
 fn solve_b(input : &str) -> u32 {
-    0
+    let program = Program::from_input(input, false);
+    (program.count() + 1) as u32
 }
 
 fn main() {
@@ -123,7 +148,51 @@ r"1
 
     #[test]
     fn b_1() {
-        let input = "blah";
-        assert_eq!(solve_b(&input), 0);
+        let input =
+r"0
+3
+0
+1
+-3";
+
+        assert_eq!(solve_b(&input), 10);
+    }
+
+    #[test]
+    fn b_2() {
+        let input = r"0";
+        assert_eq!(solve_b(&input), 2);
+    }
+
+    #[test]
+    fn b_3() {
+        let input = r"1";
+        assert_eq!(solve_b(&input), 1);
+    }
+
+    #[test]
+    fn b_4() {
+        let input =
+r"0
+0";
+        assert_eq!(solve_b(&input), 4);
+    }
+
+    #[test]
+    fn b_5() {
+        let input =
+r"1
+-1";
+        assert_eq!(solve_b(&input), 3);
+    }
+
+    #[test]
+    fn b_6() {
+        let input =
+r"3
+0
+2
+-3";
+        assert_eq!(solve_b(&input), 4);
     }
 }
