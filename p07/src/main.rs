f3d7b66a10230937ea2b340a19b305f8d2c3be55
knutaf <knutaf@gmail.com>
2018-01-12 09:44:47 -0800

    day 7 progress

--- p07/src/main.rs
+++ p07/src/main.rs
@@ -11,29 +11,49 @@ use std::collections::HashMap;
 struct ProgInfo<'t> {
     name : &'t str,
     weight : u32,
-    children : Vec<&'t str>,
+    child_names : Vec<&'t str>,
 }
 
-/*
-struct Prog {
-    name : &str,
+struct Prog<'t> {
+    name : &'t str,
     weight : u32,
-    parent : Option<Box<Prog>>,
-    children : Vec<Box<Prog>>,
+    parent : Option<Box<Prog<'t>>>,
+    children : Vec<Box<Prog<'t>>>,
 }
 
-struct ProgDb {
-    db : HashMap<&str, Prog>,
+struct ProgDb<'t> {
+    db : HashMap<&'t str, Prog<'t>>,
 }
 
-impl ProgDb {
-    fn new() -> ProgDb {
+impl<'t> ProgDb<'t> {
+    fn new() -> ProgDb<'t> {
         ProgDb {
-            db : HashMap<&str, Prog>::new(),
+            db : HashMap::<&'t str, Prog<'t>>::new(),
+        }
+    }
+
+    fn load_prog(&mut self, prog_info : ProgInfo<'t>) {
+        let mut new_prog = match self.db.get_mut(prog_info.name) {
+            None -> Prog {
+                name : prog_info.name,
+                weight : prog_info.weight,
+                parent : None,
+                children : vec![],
+            },
+            Some(prog) -> prog
+        }
+
+        for child in prog_info.child_names {
+            match self.db.get(child) {
+                None -> self.db.insert(Prog {
+                    name : child.name,
+                    weight : child.weight,
+                    parent : Some(new_prog),
+                    children : vec![],
+            }
         }
     }
 }
-*/
 
 fn parse_prog(line : &str) -> ProgInfo {
     lazy_static! {
@@ -48,7 +68,7 @@ fn parse_prog(line : &str) -> ProgInfo {
     ProgInfo {
         name : prog_info_captures.get(1).unwrap().as_str(),
         weight : prog_info_captures[2].parse::<u32>().unwrap(),
-        children : children_captures_iter.map(|caps| {
+        child_names : children_captures_iter.map(|caps| {
             caps.get(1).unwrap().as_str()
         }).collect(),
     }
@@ -83,7 +103,7 @@ mod test {
         assert_eq!(parse_prog(&input), ProgInfo {
             name: "pbga",
             weight: 66,
-            children: vec![],
+            child_names: vec![],
         });
     }
 
@@ -93,7 +113,7 @@ mod test {
         assert_eq!(parse_prog(&input), ProgInfo {
             name: "fwft",
             weight: 72,
-            children: vec!["ktlj", "cntj", "xhth"],
+            child_names: vec!["ktlj", "cntj", "xhth"],
         });
     }
 
