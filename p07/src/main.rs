a81d90d4998804f2ab6adf750245e351a0b46f36
knutaf <knutaf@gmail.com>
2018-01-17 22:15:13 -0800

    day 7 part 1 progress

--- p07/src/main.rs
+++ p07/src/main.rs
@@ -1,11 +1,16 @@
-extern crate aoclib;
-use aoclib::*;
+#![feature(nll)]
+
+use std::rc::Rc;
+use std::cell::RefCell;
+use std::cell::Ref;
+use std::collections::HashMap;
 
 #[macro_use] extern crate lazy_static;
 extern crate regex;
 use regex::Regex;
 
-use std::collections::HashMap;
+extern crate aoclib;
+use aoclib::*;
 
 #[derive(PartialEq, Debug)]
 struct ProgInfo<'t> {
@@ -14,44 +19,86 @@ struct ProgInfo<'t> {
     child_names : Vec<&'t str>,
 }
 
+type RcRefProg<'t> = Rc<RefCell<Prog<'t>>>;
+
 struct Prog<'t> {
     name : &'t str,
     weight : u32,
-    parent : Option<Box<Prog<'t>>>,
-    children : Vec<Box<Prog<'t>>>,
+    parent : Option<RcRefProg<'t>>,
+    children : Vec<RcRefProg<'t>>,
 }
 
 struct ProgDb<'t> {
-    db : HashMap<&'t str, Prog<'t>>,
+    db : HashMap<&'t str, RcRefProg<'t>>,
 }
 
 impl<'t> ProgDb<'t> {
     fn new() -> ProgDb<'t> {
         ProgDb {
-            db : HashMap::<&'t str, Prog<'t>>::new(),
+            db : HashMap::<&'t str, RcRefProg<'t>>::new(),
         }
     }
 
+    fn from_input(input : &'t str) -> ProgDb<'t> {
+        let mut pdb = ProgDb::new();
+
+        for line in input.lines() {
+            pdb.load_prog(parse_prog(line))
+        }
+
+        pdb
+    }
+
     fn load_prog(&mut self, prog_info : ProgInfo<'t>) {
-        let mut new_prog = match self.db.get_mut(prog_info.name) {
-            None -> Prog {
+        let new_prog = match self.db.get(prog_info.name) {
+            None => Rc::new(RefCell::new(Prog {
                 name : prog_info.name,
                 weight : prog_info.weight,
                 parent : None,
                 children : vec![],
-            },
-            Some(prog) -> prog
+            })),
+            Some(prog) => prog.clone(),
+        };
+
+        new_prog.borrow_mut().weight = prog_info.weight;
+
+        for child_name in prog_info.child_names {
+            let child_prog = match self.db.get(child_name) {
+                None => {
+                    let new_child = Rc::new(RefCell::new(Prog {
+                        name : child_name,
+                        weight : 0,
+                        parent : Some(new_prog.clone()),
+                        children : vec![],
+                    }));
+
+                    self.db.insert(child_name, new_child.clone());
+
+                    new_child
+                },
+                Some(cp) => {
+                    let mut existing_child_prog = cp.clone();
+                    cp.borrow_mut().parent = Some(new_prog.clone());
+                    existing_child_prog
+                }
+            };
+
+            new_prog.borrow_mut().children.push(child_prog);
         }
 
-        for child in prog_info.child_names {
-            match self.db.get(child) {
-                None -> self.db.insert(Prog {
-                    name : child.name,
-                    weight : child.weight,
-                    parent : Some(new_prog),
-                    children : vec![],
-            }
-        }
+        self.db.insert(prog_info.name, new_prog);
+    }
+
+    fn get(&self, prog_name : &str) -> Option<Ref<Prog<'t>>> {
+        self.db.get(prog_name).map(|v| {
+            v.borrow()
+        })
+    }
+
+    fn get_root(&self) -> Ref<Prog<'t>> {
+        self.db.values().find(|prog| {
+            prog.borrow().parent.is_none()
+        }).unwrap().borrow()
     }
 }
 
@@ -115,6 +162,56 @@ mod test {
             weight: 72,
             child_names: vec!["ktlj", "cntj", "xhth"],
         });
+    }
+
+    #[test]
+    fn weights_in_order() {
+        let input =
+r"b (1)
+c (2)
+d (3)
+a (100) -> b, c, d";
+        let db = ProgDb::from_input(input);
+        assert_eq!(db.get("a").unwrap().weight, 100);
+        assert_eq!(db.get("b").unwrap().weight, 1);
+        assert_eq!(db.get("c").unwrap().weight, 2);
+        assert_eq!(db.get("d").unwrap().weight, 3);
+    }
+
+    #[test]
+    fn weights_backwards() {
+        let input =
+r"a (100) -> b, c, d
+b (1)
+c (2)
+d (3)";
+        let db = ProgDb::from_input(input);
+        assert_eq!(db.get("a").unwrap().weight, 100);
+        assert_eq!(db.get("b").unwrap().weight, 1);
+        assert_eq!(db.get("c").unwrap().weight, 2);
+        assert_eq!(db.get("d").unwrap().weight, 3);
+    }
+
+    #[test]
+    fn root_1() {
+        let input =
+r"a (100) -> b, c, d
+b (1)
+c (2)
+d (3)";
+        let db = ProgDb::from_input(input);
+        assert_eq!(db.get_root().name, "a");
+    }
+
+    #[test]
+    fn root_2() {
+        let input =
+r"b (1)
+c (2)
+d (3)
+a (100) -> b, c, d";
+        let db = ProgDb::from_input(input);
+        assert_eq!(db.get_root().name, "a");
     }
 
     #[test]
