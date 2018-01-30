d7cf12e10ef02f22b18d30193d0b0e1b07ce65f4
knutaf <knutaf@gmail.com>
2018-01-30 08:38:02 -0800

    day 9 part 1

--- p09/src/main.rs
+++ p09/src/main.rs
@@ -1,8 +1,75 @@
 extern crate aoclib;
 use aoclib::*;
 
+enum State {
+    Normal,
+    Garbage,
+    GarbageCancel,
+}
+
+struct GroupCounter<'t> {
+    chars : std::str::Chars<'t>,
+    state : State,
+    score : u32,
+    depth : u32,
+}
+
+impl<'t> GroupCounter<'t> {
+    fn over(input : &'t str) -> GroupCounter<'t> {
+        GroupCounter {
+            chars : input.chars(),
+            state : State::Normal,
+            score : 0,
+            depth : 0,
+        }
+    }
+}
+
+impl<'t> Iterator for GroupCounter<'t> {
+    type Item = u32;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        self.chars.next().map(|ch| {
+            match self.state {
+                State::Normal => {
+                    match ch {
+                        '{' => {
+                            self.depth += 1;
+                        },
+                        '}' => {
+                            self.score += self.depth;
+                            self.depth -= 1;
+                        },
+                        '<' => {
+                            self.state = State::Garbage;
+                        },
+                        _ => {},
+                    }
+                },
+                State::Garbage => {
+                    match ch {
+                        '>' => {
+                            self.state = State::Normal;
+                        },
+                        '!' => {
+                            self.state = State::GarbageCancel;
+                        },
+                        _ => {},
+                    }
+                },
+                State::GarbageCancel => {
+                    self.state = State::Garbage;
+                },
+            }
+
+            self.score
+        })
+    }
+}
+
 fn solve_a(input : &str) -> u32 {
-    0
+    let counter = GroupCounter::over(input);
+    counter.last().unwrap_or(0)
 }
 
 fn solve_b(input : &str) -> u32 {
@@ -26,8 +93,15 @@ mod test {
 
     #[test]
     fn a_1() {
-        let input = "blah";
-        assert_eq!(solve_a(&input), 0);
+        assert_eq!(solve_a("{}"), 1);
+        assert_eq!(solve_a("{{{}}}"), 6);
+        assert_eq!(solve_a("{{},{}}"), 5);
+        assert_eq!(solve_a("{{{},{},{{}}}}"), 16);
+        assert_eq!(solve_a("{<{},{},{{}}>}"), 1);
+        assert_eq!(solve_a("{<a>,<a>,<a>,<a>}"), 1);
+        assert_eq!(solve_a("{{<ab>},{<ab>},{<ab>},{<ab>}}"), 9);
+        assert_eq!(solve_a("{{<!!>},{<!!>},{<!!>},{<!!>}}"), 9);
+        assert_eq!(solve_a("{{<a!>},{<a!>},{<a!>},{<ab>}}"), 3);
     }
 
     #[test]
