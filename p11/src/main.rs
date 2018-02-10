a4a99fa4ea81a2cd2a1477503236128146efdc4e
knutaf <knutaf@gmail.com>
2018-02-10 09:01:34 -0800

    day 11 part 1 progress

--- p11/src/main.rs
+++ p11/src/main.rs
@@ -1,5 +1,50 @@
+#![feature(nll)]
+#![feature(dyn_trait)]
+
 extern crate aoclib;
 use aoclib::*;
+
+struct HexMover<'t> {
+    pos : (i32, i32),
+    steps : Vec<&'t str>,
+    index : usize,
+}
+
+impl<'t> HexMover<'t> {
+    fn new(steps : &'t str) -> HexMover<'t> {
+        let steps = steps.split(',').collect();
+
+        HexMover {
+            pos : (0, 0),
+            steps : steps,
+            index : 0,
+        }
+    }
+}
+
+impl<'t> Iterator for HexMover<'t> {
+    type Item = (i32, i32);
+
+    fn next(&mut self) -> Option<Self::Item> {
+        if self.index < self.steps.len() {
+            self.pos = match self.steps[self.index] {
+                "n" => (self.pos.0, self.pos.1 + 2),
+                "s" => (self.pos.0, self.pos.1 - 2),
+                "ne" => (self.pos.0 + 2, self.pos.1 + 1),
+                "se" => (self.pos.0 + 2, self.pos.1 - 1),
+                "nw" => (self.pos.0 - 2, self.pos.1 + 1),
+                "sw" => (self.pos.0 - 2, self.pos.1 - 1),
+                _ => panic!("invalid step {}", self.steps[self.index]),
+            };
+
+            self.index += 1;
+
+            Some(self.pos)
+        } else {
+            None
+        }
+    }
+}
 
 fn solve_a(input : &str) -> u32 {
     0
@@ -24,10 +69,14 @@ fn main() {
 mod test {
     use super::*;
 
+    fn test_coords(input : &str, final_pos : (i32, i32)) {
+        let mover = HexMover::new(input);
+        assert_eq!(mover.last().unwrap(), final_pos);
+    }
+
     #[test]
     fn a_1() {
-        let input = "blah";
-        assert_eq!(solve_a(&input), 0);
+        test_coords("ne,sw", (0, 0));
     }
 
     #[test]
