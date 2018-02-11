5ac40ebe03d98d3cfe52782ae6b88131bee74953
knutaf <knutaf@gmail.com>
2018-02-10 17:26:49 -0800

    day 13 part 1 start

---
+++ p13/src/main.rs
@@ -0,0 +1,74 @@
+#![feature(nll)]
+
+extern crate aoclib;
+use aoclib::*;
+
+struct Layer {
+    depth : u32,
+    range : u32,
+}
+
+struct Firewall {
+    layers : Vec<Option<Layer>>,
+}
+
+impl Firewall {
+    fn from(input : &str) -> Firewall {
+        let mut layers = vec![];
+
+        for line in input.lines() {
+            let split : Vec<&str> = line.split(": ").collect();
+
+            let layer = Layer {
+                depth : split[0].parse::<u32>().unwrap(),
+                range : split[1].parse::<u32>().unwrap(),
+            };
+
+            while layers.len() < (layer.depth as usize) {
+                layers.push(None);
+            }
+
+            layers.push(Some(layer));
+        }
+
+        Firewall {
+            layers : layers,
+        }
+    }
+}
+
+fn solve_a(input : &str) -> u32 {
+    0
+}
+
+fn solve_b(input : &str) -> u32 {
+    0
+}
+
+fn main() {
+    let input = read_all_stdin();
+    //eprintln!("input: {}", input);
+
+    if aoclib::should_solve_puzzle_a() {
+        println!("answer: {}", solve_a(&input));
+    } else {
+        println!("answer: {}", solve_b(&input));
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn a_1() {
+        let input = "blah";
+        assert_eq!(solve_a(&input), 0);
+    }
+
+    #[test]
+    fn b_1() {
+        let input = "blah";
+        assert_eq!(solve_b(&input), 0);
+    }
+}
