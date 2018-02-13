7f7cff992eeccbb96931f8848a7d71fc27686654
knutaf <knutaf@gmail.com>
2018-02-13 09:06:04 -0800

    day 13 part 1

--- p13/src/main.rs
+++ p13/src/main.rs
@@ -12,6 +12,12 @@ struct Firewall {
     layers : Vec<Option<Layer>>,
 }
 
+struct PacketPasser<'t> {
+    firewall : std::slice::Iter<'t, Option<Layer>>,
+    t : u32,
+    cost : u32,
+}
+
 impl Firewall {
     fn from(input : &str) -> Firewall {
         let mut layers = vec![];
@@ -35,10 +41,58 @@ impl Firewall {
             layers : layers,
         }
     }
+
+    fn simulate<'t>(&'t self) -> PacketPasser<'t> {
+        PacketPasser {
+            firewall : self.layers.iter(),
+            t : 0,
+            cost : 0,
+        }
+    }
+
+    fn wrap_scanner(t : u32, range : u32) -> u32 {
+        if range > 1 {
+            // The scanner goes back and forth over the range// so unwrap to 2x the range. But the
+            // endpoints don't quite count, so subtract them both.
+            let unwrapped_range = (range * 2) - 2;
+            let unwrapped_position = t % unwrapped_range;
+
+            // If the scanner is within the range itself, just use that position. If it's in the
+            // unwrapped part of the range, subtract the unwrapped range to get it back into the
+            // regular range.
+            if unwrapped_position < range {
+                unwrapped_position
+            } else {
+                unwrapped_range - unwrapped_position
+            }
+        } else {
+            0
+        }
+    }
 }
 
+impl<'t> Iterator for PacketPasser<'t> {
+    type Item = u32;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        self.firewall.next().map(|layer| {
+            if let Some(layer) = layer.as_ref() {
+                if Firewall::wrap_scanner(self.t, layer.range) == 0 {
+                    self.cost += layer.depth * layer.range;
+                }
+            }
+
+            self.t += 1;
+
+            self.cost
+        })
+    }
+}
+
+
 fn solve_a(input : &str) -> u32 {
-    0
+    let firewall = Firewall::from(input);
+    firewall.simulate().inspect(|x| {eprintln!("{}", x);}).last().unwrap()
 }
 
 fn solve_b(input : &str) -> u32 {
@@ -61,8 +115,66 @@ mod test {
     use super::*;
 
     #[test]
-    fn a_1() {
-        let input = "blah";
+    fn wrap_scanner_3() {
+       assert_eq!(Firewall::wrap_scanner(0, 3), 0);
+       assert_eq!(Firewall::wrap_scanner(1, 3), 1);
+       assert_eq!(Firewall::wrap_scanner(2, 3), 2);
+       assert_eq!(Firewall::wrap_scanner(3, 3), 1);
+       assert_eq!(Firewall::wrap_scanner(4, 3), 0);
+       assert_eq!(Firewall::wrap_scanner(5, 3), 1);
+       assert_eq!(Firewall::wrap_scanner(6, 3), 2);
+       assert_eq!(Firewall::wrap_scanner(7, 3), 1);
+       assert_eq!(Firewall::wrap_scanner(8, 3), 0);
+    }
+
+    #[test]
+    fn wrap_scanner_6() {
+       assert_eq!(Firewall::wrap_scanner(0, 6), 0);
+       assert_eq!(Firewall::wrap_scanner(1, 6), 1);
+       assert_eq!(Firewall::wrap_scanner(2, 6), 2);
+       assert_eq!(Firewall::wrap_scanner(3, 6), 3);
+       assert_eq!(Firewall::wrap_scanner(4, 6), 4);
+       assert_eq!(Firewall::wrap_scanner(5, 6), 5);
+       assert_eq!(Firewall::wrap_scanner(6, 6), 4);
+       assert_eq!(Firewall::wrap_scanner(7, 6), 3);
+       assert_eq!(Firewall::wrap_scanner(8, 6), 2);
+       assert_eq!(Firewall::wrap_scanner(9, 6), 1);
+       assert_eq!(Firewall::wrap_scanner(10, 6), 0);
+       assert_eq!(Firewall::wrap_scanner(11, 6), 1);
+       assert_eq!(Firewall::wrap_scanner(12, 6), 2);
+    }
+
+    #[test]
+    fn a_given() {
+        let input =
+r"0: 3
+1: 2
+4: 4
+6: 4";
+        assert_eq!(solve_a(&input), 24);
+    }
+
+    #[test]
+    fn a_all_caught() {
+        let input =
+r"0: 1
+1: 1
+2: 1
+3: 1
+4: 1
+5: 1";
+        assert_eq!(solve_a(&input), 15);
+    }
+
+    #[test]
+    fn a_none_caught() {
+        let input =
+r"0: 1
+1: 2
+2: 3
+3: 4
+4: 5
+5: 6";
         assert_eq!(solve_a(&input), 0);
     }
 
