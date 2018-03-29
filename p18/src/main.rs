518bd6ef31f6e83e189e03c3133828f8968baba4
knutaf <knutaf@gmail.com>
2018-03-28 17:20:46 -0700

    day 18 part 2 progress

--- p18/src/main.rs
+++ p18/src/main.rs
@@ -20,7 +20,7 @@ enum Instruction {
     Add(char, RegisterOrValue),
     Mul(char, RegisterOrValue),
     Mod(char, RegisterOrValue),
-    Rcv(RegisterOrValue),
+    Rcv(char),
     Jgz(RegisterOrValue, RegisterOrValue),
 }
 
@@ -28,12 +28,30 @@ struct Program {
     instructions : Vec<Instruction>,
 }
 
+struct RegisterHolder {
+    registers : [i64 ; ((('z' as u8) - ('a' as u8)) + 1) as usize],
+}
+
 struct Execution<'t> {
     instructions : &'t Vec<Instruction>,
     position : usize,
-    registers : [i64 ; ((('z' as u8) - ('a' as u8)) + 1) as usize],
+    registers : RegisterHolder,
     last_freq : i64,
     last_recovery : Option<i64>,
+}
+
+struct ExecutionStep {
+    is_blocked : bool,
+    new_snd : Option<i64>,
+}
+
+struct ExecutionB<'t> {
+    id : i64,
+    instructions : &'t Vec<Instruction>,
+    position : usize,
+    registers : RegisterHolder,
+    rcv_queue : Vec<i64>,
+    snd_count : u32,
 }
 
 impl RegisterOrValue {
@@ -70,7 +88,7 @@ impl Instruction {
             static ref RE_ADD : regex::Regex = Regex::new(r"^add ([a-zA-Z]) (.*)$").expect("failed to compile regex");
             static ref RE_MUL : regex::Regex = Regex::new(r"^mul ([a-zA-Z]) (.*)$").expect("failed to compile regex");
             static ref RE_MOD : regex::Regex = Regex::new(r"^mod ([a-zA-Z]) (.*)$").expect("failed to compile regex");
-            static ref RE_RCV : regex::Regex = Regex::new(r"^rcv (.*)$").expect("failed to compile regex");
+            static ref RE_RCV : regex::Regex = Regex::new(r"^rcv ([a-zA-Z])$").expect("failed to compile regex");
             static ref RE_JGZ : regex::Regex = Regex::new(r"^jgz (.*) (.*)$").expect("failed to compile regex");
         }
 
@@ -87,7 +105,7 @@ impl Instruction {
         } else if let Some(captures) = RE_JGZ.captures_iter(input).next() {
             Instruction::Jgz(RegisterOrValue::from(captures.get(1).unwrap().as_str()), RegisterOrValue::from(captures.get(2).unwrap().as_str()))
         } else if let Some(captures) = RE_RCV.captures_iter(input).next() {
-            Instruction::Rcv(RegisterOrValue::from(captures.get(1).unwrap().as_str()))
+            Instruction::Rcv(captures.get(1).unwrap().as_str().chars().nth(0).unwrap())
         } else {
             panic!("invalid move {}", input);
         }
@@ -116,13 +134,11 @@ impl Program {
     }
 
     fn execute<'t>(&'t self) -> Execution<'t> {
-        Execution {
-            instructions : &self.instructions,
-            position : 0,
-            registers : [0 ; ((('z' as u8) - ('a' as u8)) + 1) as usize],
-            last_freq : 0,
-            last_recovery : None,
-        }
+        Execution::new(&self.instructions)
+    }
+
+    fn execute_b<'t>(&'t self, program_id : i64) -> ExecutionB<'t> {
+        ExecutionB::new(&self.instructions, program_id)
     }
 }
 
@@ -136,7 +152,13 @@ impl fmt::Display for Program {
     }
 }
 
-impl<'t> Execution<'t> {
+impl RegisterHolder {
+    fn new() -> RegisterHolder {
+        RegisterHolder {
+            registers : [0 ; ((('z' as u8) - ('a' as u8)) + 1) as usize],
+        }
+    }
+
     fn get_reg_mut(&mut self, reg : char) -> &mut i64 {
         &mut self.registers[reg as usize - 'a' as usize]
     }
@@ -157,6 +179,18 @@ impl<'t> Execution<'t> {
     }
 }
 
+impl<'t> Execution<'t> {
+    fn new(instructions : &'t Vec<Instruction>) -> Execution<'t> {
+        Execution {
+            instructions : instructions,
+            position : 0,
+            registers : RegisterHolder::new(),
+            last_freq : 0,
+            last_recovery : None,
+        }
+    }
+}
+
 impl<'t> Iterator for Execution<'t> {
     type Item = Option<i64>;
 
@@ -167,27 +201,27 @@ impl<'t> Iterator for Execution<'t> {
 
             match inst {
                 &Instruction::Snd(ref rv) => {
-                    self.last_freq = self.evaluate(&rv);
+                    self.last_freq = self.registers.evaluate(&rv);
                     eprintln!("  last_freq = {}", self.last_freq);
                 }
                 &Instruction::Set(ref reg, ref rv) => {
-                    eprintln!("  {} <= {}", reg, self.evaluate(&rv));
-                    *self.get_reg_mut(*reg) = self.evaluate(&rv);
+                    eprintln!("  {} <= {}", reg, self.registers.evaluate(&rv));
+                    *self.registers.get_reg_mut(*reg) = self.registers.evaluate(&rv);
                 }
                 &Instruction::Add(ref reg, ref rv) => {
-                    eprintln!("  add {} {} ({})", *self.get_reg(*reg), self.evaluate(&rv), *self.get_reg(*reg) + self.evaluate(&rv));
-                    *self.get_reg_mut(*reg) = *self.get_reg(*reg) + self.evaluate(&rv);
+                    eprintln!("  add {} {} ({})", *self.registers.get_reg(*reg), self.registers.evaluate(&rv), *self.registers.get_reg(*reg) + self.registers.evaluate(&rv));
+                    *self.registers.get_reg_mut(*reg) = *self.registers.get_reg(*reg) + self.registers.evaluate(&rv);
                 }
                 &Instruction::Mul(ref reg, ref rv) => {
-                    eprintln!("  mul {} {} ({})", *self.get_reg(*reg), self.evaluate(&rv), *self.get_reg(*reg) * self.evaluate(&rv));
-                    *self.get_reg_mut(*reg) = *self.get_reg(*reg) * self.evaluate(&rv);
+                    eprintln!("  mul {} {} ({})", *self.registers.get_reg(*reg), self.registers.evaluate(&rv), *self.registers.get_reg(*reg) * self.registers.evaluate(&rv));
+                    *self.registers.get_reg_mut(*reg) = *self.registers.get_reg(*reg) * self.registers.evaluate(&rv);
                 }
                 &Instruction::Mod(ref reg, ref rv) => {
-                    eprintln!("  mod {} {} ({})", *self.get_reg(*reg), self.evaluate(&rv), *self.get_reg(*reg) % self.evaluate(&rv));
-                    *self.get_reg_mut(*reg) = *self.get_reg(*reg) % self.evaluate(&rv);
+                    eprintln!("  mod {} {} ({})", *self.registers.get_reg(*reg), self.registers.evaluate(&rv), *self.registers.get_reg(*reg) % self.registers.evaluate(&rv));
+                    *self.registers.get_reg_mut(*reg) = *self.registers.get_reg(*reg) % self.registers.evaluate(&rv);
                 }
-                &Instruction::Rcv(ref rv) => {
-                    if self.evaluate(&rv) != 0 {
+                &Instruction::Rcv(ref reg) => {
+                    if self.registers.evaluate(&RegisterOrValue::Reg(*reg)) != 0 {
                         eprintln!("  set to {}", self.last_freq);
                         self.last_recovery = Some(self.last_freq);
                     } else {
@@ -199,8 +233,8 @@ impl<'t> Iterator for Execution<'t> {
 
             self.position = match inst {
                 &Instruction::Jgz(ref cond, ref jump_offset) => {
-                    if self.evaluate(&cond) > 0 {
-                        let offset = self.evaluate(&jump_offset);
+                    if self.registers.evaluate(&cond) > 0 {
+                        let offset = self.registers.evaluate(&jump_offset);
                         if offset >= 0 {
                             self.position + (offset as usize)
                         } else {
@@ -225,6 +259,113 @@ impl<'t> Iterator for Execution<'t> {
             };
 
             Some(self.last_recovery)
+        } else {
+            None
+        }
+    }
+}
+
+impl<'t> ExecutionB<'t> {
+    fn new(instructions : &'t Vec<Instruction>, program_id : i64) -> ExecutionB<'t> {
+        let mut exec = ExecutionB {
+            id : program_id,
+            instructions : instructions,
+            position : 0,
+            registers : RegisterHolder::new(),
+            rcv_queue : Vec::new(),
+            snd_count : 0,
+        };
+
+        *exec.registers.get_reg_mut('p') = program_id;
+
+        exec
+    }
+
+    fn get_snd_count(&self) -> u32 {
+        self.snd_count
+    }
+}
+
+impl<'t> Iterator for ExecutionB<'t> {
+    type Item = ExecutionStep;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        if self.position < self.instructions.len() {
+            let inst = &self.instructions[self.position];
+            eprintln!("{}: {}", self.position, inst);
+
+            let mut is_blocked = false;
+            let mut new_snd = None;
+
+            match inst {
+                &Instruction::Snd(ref rv) => {
+                    new_snd = Some(self.registers.evaluate(&rv));
+                    eprintln!("  sending {:?}", new_snd.as_ref());
+                }
+                &Instruction::Set(ref reg, ref rv) => {
+                    eprintln!("  {} <= {}", reg, self.registers.evaluate(&rv));
+                    *self.registers.get_reg_mut(*reg) = self.registers.evaluate(&rv);
+                }
+                &Instruction::Add(ref reg, ref rv) => {
+                    eprintln!("  add {} {} ({})", *self.registers.get_reg(*reg), self.registers.evaluate(&rv), *self.registers.get_reg(*reg) + self.registers.evaluate(&rv));
+                    *self.registers.get_reg_mut(*reg) = *self.registers.get_reg(*reg) + self.registers.evaluate(&rv);
+                }
+                &Instruction::Mul(ref reg, ref rv) => {
+                    eprintln!("  mul {} {} ({})", *self.registers.get_reg(*reg), self.registers.evaluate(&rv), *self.registers.get_reg(*reg) * self.registers.evaluate(&rv));
+                    *self.registers.get_reg_mut(*reg) = *self.registers.get_reg(*reg) * self.registers.evaluate(&rv);
+                }
+                &Instruction::Mod(ref reg, ref rv) => {
+                    eprintln!("  mod {} {} ({})", *self.registers.get_reg(*reg), self.registers.evaluate(&rv), *self.registers.get_reg(*reg) % self.registers.evaluate(&rv));
+                    *self.registers.get_reg_mut(*reg) = *self.registers.get_reg(*reg) % self.registers.evaluate(&rv);
+                }
+                &Instruction::Rcv(ref reg) => {
+                    if let Some(val) = self.rcv_queue.pop() {
+                        *self.registers.get_reg_mut(*reg) = val;
+                        eprintln!("  rcv {} into {}", val, reg);
+                    } else {
+                        is_blocked = true;
+                        eprintln!("  blocked on rcv into {}", reg);
+                    }
+                }
+                &Instruction::Jgz(..) => {},
+            }
+
+            self.position =
+            if is_blocked {
+                self.position
+            } else {
+                match inst {
+                    &Instruction::Jgz(ref cond, ref jump_offset) => {
+                        if self.registers.evaluate(&cond) > 0 {
+                            let offset = self.registers.evaluate(&jump_offset);
+                            if offset >= 0 {
+                                self.position + (offset as usize)
+                            } else {
+                                if ((offset * -1) as usize) <= self.position {
+                                    ((self.position as i64) + offset) as usize
+                                } else {
+                                    self.instructions.len()
+                                }
+                            }
+                        } else {
+                            self.position + 1
+                        }
+                    },
+                    &Instruction::Snd(..) |
+                    &Instruction::Set(..) |
+                    &Instruction::Add(..) |
+                    &Instruction::Mul(..) |
+                    &Instruction::Mod(..) |
+                    &Instruction::Rcv(..) => {
+                        self.position + 1
+                    },
+                }
+            };
+
+            Some(ExecutionStep {
+                is_blocked,
+                new_snd,
+            })
         } else {
             None
         }
