b8b83b0484a476b7ffe6ef576366da248c81d2a0
knutaf <knutaf@gmail.com>
2018-05-17 12:34:18 -0700

    progress

--- p22/src/main.rs
+++ p22/src/main.rs
@@ -1,8 +1,66 @@
 #![feature(nll)]
 
+use std::collections::HashSet;
+use std::hash::{Hash, Hasher};
+
 extern crate aoclib;
 use aoclib::*;
 use aoclib::onoffpixel::OnOffPixel;
+
+struct PixelInGrid {
+    pos : (i32, i32),
+    value : OnOffPixel,
+}
+
+struct WormProgress {
+    pixels : HashSet<PixelInGrid>,
+    pos : (i32, i32),
+    num_activated : u32,
+}
+
+impl PartialEq for PixelInGrid {
+    fn eq(&self, other: &PixelInGrid) -> bool {
+        self.pos == other.pos
+    }
+}
+
+impl Eq for PixelInGrid {}
+
+impl Hash for PixelInGrid {
+    fn hash<H>(&self, state : &mut H)
+    where H : Hasher {
+        self.pos.hash(state)
+    }
+}
+
+impl WormProgress {
+    fn load(input : &str) -> WormProgress {
+        let offset = ((input.lines().nth(0).unwrap().len() / 2) - 1) as i32;
+        let mut pixels = HashSet::new();
+
+        for (y, line) in input.lines().enumerate() {
+            for (x, ch) in line.chars().enumerate() {
+                let value = OnOffPixel::parse(ch);
+                if value == OnOffPixel::On {
+                    pixels.insert(PixelInGrid {
+                        pos : (offset - (x as i32), (offset - (y as i32))),
+                        value,
+                    });
+                }
+            }
+        }
+
+        WormProgress {
+            pixels,
+            pos : (0, 0),
+            num_activated : 0,
+        }
+    }
+}
+
+fn count_infected(input : &str, iterations : usize) -> usize {
+    0
+}
 
 fn solve_a(input : &str) -> u32 {
     0
@@ -28,9 +86,15 @@ mod test {
     use super::*;
 
     #[test]
-    fn a_1() {
-        let input = "blah";
-        assert_eq!(solve_a(&input), 0);
+    fn a_given() {
+        let input =
+r"..#
+#..
+...";
+        assert_eq!(count_infected(&input, 0), 0);
+        assert_eq!(count_infected(&input, 7), 5);
+        assert_eq!(count_infected(&input, 70), 41);
+        assert_eq!(solve_a(&input), 5587);
     }
 
     #[test]
